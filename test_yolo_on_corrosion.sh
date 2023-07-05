93d4979d1b4d19136f84df1019ecedfb6fb461d9
David Carlyn <davidecarlyn@gmail.com>
2023-07-05 12:05:35 -0400

    Added updated training scripts

---
+++ test_yolo_on_corrosion.sh
@@ -0,0 +1,4 @@
+python /home/carlyn.1/object_detection/externals/yolov7_seg/seg/segment/val.py \
+    --data /home/carlyn.1/object_detection/data/our_corrosion/corrosion.yaml \
+    --weights '/home/carlyn.1/object_detection/externals/yolov7_seg/seg/runs/train-seg/yolov7-our_corrosion_seg2/weights/best.pt' \
+    --name yolov7-corrosion_seg --img 640 --batch-size 1
