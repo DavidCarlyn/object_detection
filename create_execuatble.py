5ebf3eff074f6a524b17b4f4c571f0dda142c849
David Carlyn <davidecarlyn@gmail.com>
2023-08-04 15:03:42 -0400

    Created environment.yml file and added flexibility to create executable script

--- create_execuatble.py
+++ create_execuatble.py
@@ -1,22 +1,28 @@
 import PyInstaller.__main__
 from argparse import ArgumentParser
 
-def create_executable(site_packages_path="virtual_environments/det1/Lib/site-packages"):
-    PyInstaller.__main__.run([
+def create_executable(site_packages_path=None, exe_type="onefile"):
+    cmds = [
         "main.py",
         "--add-data", "externals;externals",
-        "--paths", site_packages_path,
         "--hidden-import", "seaborn",
         "--hidden-import", "scipy",
         "--hidden-import", "scipy.signal",
         "--exclude-module", "test", # Yolov7 imports a local test which conflicts with the default test
-        "--onefile",
         "--console",
         "--noconfirm"
-    ])
+    ]
+
+    if site_packages_path is not None:
+        cmds += ["--paths", site_packages_path]
+
+    cmds += [f"--{exe_type}"]
+
+    PyInstaller.__main__.run(cmds)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--site-packages", type=str, default="virtual_environments/test/det1/site-packages")
+    parser.add_argument("--site-packages", type=str, default=None)
+    parser.add_argument("--exe_type", type=str, default="onefile")
     args = parser.parse_args()
-    create_executable(site_packages_path=args.site_packages)
+    create_executable(site_packages_path=args.site_packages, exe_type=args.exe_type)
